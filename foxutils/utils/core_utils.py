7dcf055a7790d0d988ad4afa00d5566addb969fb
foxelas <32893423+foxelas@users.noreply.github.com>
2023-08-01 19:05:36 +0300

    Update core_utils.py

--- foxutils/utils/core_utils.py
+++ foxutils/utils/core_utils.py
@@ -32,11 +32,13 @@ from torchvision import transforms
 ###########################################################
 
 SEED = 42
-os.environ['PYTHONHASHSEED'] = str(SEED)
-os.environ['IF_CUDNN_DETERMINISTIC'] = '1'  # for tf 2.0+
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.seed(SEED)
+
+def initSeed(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['IF_CUDNN_DETERMINISTIC'] = '1'  # for tf 2.0+
+    random.seed(seed)
+    rng = np.random.default_rng(seed)
+    tf.random.seed(seed)
 
 ###########################################################
 # The filename of the settings file
