bb8c8ea31f56e471e9e33a9e11320a47c83d57d1
foxelas <foxelas@outlook.com>
2023-11-23 12:31:05 +0200

    Update monitoring_utils.py

--- utils/monitoring_utils.py
+++ utils/monitoring_utils.py
@@ -6,34 +6,46 @@ import pandas as pd
 ###########################################################
 # GPU
 
-def show_gpu_settings():
-    import torch
+def show_gpu_settings_torch():
+    try:
+        import torch
 
-    print("\nCheck Torch version:", torch.__version__)
-    print("GPU availability:", torch.cuda.is_available())
+        print("\nCheck Torch version:", torch.__version__)
+        print("GPU availability:", torch.cuda.is_available())
 
-    # setting device on GPU if available, else CPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Using device:", device)
-    print("CUDA version:", torch.version.cuda)
+        # setting device on GPU if available, else CPU
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("Using device:", device)
+        print("CUDA version:", torch.version.cuda)
 
-    # Additional Info when using cuda
-    if device.type == "cuda":
-        print("Number of GPU devices:", torch.cuda.device_count())
-        print("Name of current GPU:", torch.cuda.get_device_name(0))
-        print("Memory Usage:")
-        print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), "GB")
-        print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), "GB")
+        # Additional Info when using cuda
+        if device.type == "cuda":
+            print("Number of GPU devices:", torch.cuda.device_count())
+            print("Name of current GPU:", torch.cuda.get_device_name(0))
+            print("Memory Usage:")
+            print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), "GB")
+            print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), "GB")
+    except ModuleNotFoundError:
+        print("Torch is not installed.")
 
-
-def test_gpu(values=["torch", "tensorflow"]):
-    if "torch" in values:
-        show_gpu_settings()
-
-    if "tensorflow" in values:
+def show_gpu_settings_tensorflow():
+    try:
         import tensorflow as tf
         print(f"\nCheck tensorflow version: {tf.__version__}")
         print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    except ModuleNotFoundError:
+        print("Tensorflow is not installed.")
+
+
+def test_gpu(values=["torch", "tensorflow"]):
+    print("Testing GPU settings...")
+    if "torch" in values:
+        show_gpu_settings_torch()
+        print("")
+
+    if "tensorflow" in values:
+        show_gpu_settings_tensorflow()
+        print("")
 
 ###########################################################
 # Memory
